e4360edc2e40643f139b79e7d54b62e2dfb99079
Virus5600 <satchi5600@gmail.com>
2023-02-19 06:46:33 +0800

    Hotfix 20230219 - Quick Heroku Patches

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,30 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use URL;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		// Set pagination to bootstrap
+		Paginator::useBootstrap();
+
+		// Use https when on peoduction
+		if(config('app.env') === 'production' || config('app.env') === 'staging') {
+			URL::forceScheme('https');
+		}
+	}
 }
