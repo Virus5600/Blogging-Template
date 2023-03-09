4d92e6c58e96ff90757ca751943e9114544124c2
Virus5600 <satchi5600@gmail.com>
2023-03-09 20:53:04 +0800

    Update SecureHeaders.php

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -20,7 +20,8 @@ class SecureHeaders
 		'X-Frame-Options' => 'sameorigin',
 		'X-Content-Type-Options' => 'nosniff',
 		'X-XSS-Protection' => '1; mode=block',
-		'Strict-Transport-Security' => 'max-age:31536000; includeSubDomains'
+		'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains;',
+		'HSTS' => 'max-age=31536000; includeSubDomains;'
 	];
 
 	/**
