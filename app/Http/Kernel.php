bab26ddc23df39a5f47739e1f8d324fc4330a8b1
Virus5600 <satchi5600@gmail.com>
2023-03-09 09:48:59 +0800

    Revert "Update 20230309 - Further Security Fixes"
    Reverts commit 1e5af9f
    
    [CHANGELOG]:
    + Re-added local Secure Headers middleware
    * Reverted back composer
    - Removed Bepsvt's Secure Header package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Spatie\Csp\AddCspHeaders::class,
-        \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
+        \App\Http\Middleware\SecureHeaders::class,
     ];
 
     /**
---
+++ app/Http/Middleware/SecureHeaders.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use Closure;
+
+class SecureHeaders
+{
+	// Enumerate unwanted headers
+	private $unwantedHeaderList = [
+		'X-Powered-By',
+		'Server',
+	];
+
+	private $wantedHeaderList = [
+		'X-Content-Type-Options' => 'nosniff',
+		'X-XSS-Protection' => '1; mode=block',
+		'Strict-Transport-Security' => 'max-age:31536000; includeSubDomains'
+	];
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  Request  $request
+	 * @param  Closure  $next
+	 * @return Response
+	 */
+	public function handle(Request $request, Closure $next) {
+		if (!headers_sent()) {
+			$response = $next($request);
+
+			// SETTER
+			foreach ($this->wantedHeaderList as $header => $value) {
+				header("{$header}:{$value}");
+				$response->headers->set($header, $value, true);
+			}
+
+			// REMOVER
+			foreach ($this->unwantedHeaderList as $header)
+				header_remove($header);
+		}
+		
+		return $next($request);
+	}
+}
