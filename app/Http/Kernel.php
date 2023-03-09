1e5af9ffcfd1147f0fc9744932b2d99c20a0b0dd
Virus5600 <satchi5600@gmail.com>
2023-03-09 09:29:26 +0800

    Update 20230309 - Further Security Fixes
    + Added Bepsvt's Secure Header package
    * Replaced local Secure Header middleware in favor of the Secure Header middleware from Bepsvpt's Secure Headers package
    - Removed logger from `copy-to-clipboard.js`
    - Removed local Secure Headers middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Spatie\Csp\AddCspHeaders::class,
-        \App\Http\Middleware\SecureHeaders::class,
+        \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
     ];
 
     /**
