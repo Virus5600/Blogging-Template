0ce5dc6eec3fe7fffe6676cca94352f9a71e52ca
Virus5600 <satchi5600@gmail.com>
2023-03-10 04:27:07 +0800

    Update 20230309 - Further Security Fixes

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -21,7 +21,7 @@ class SecureHeaders
 		'X-Content-Type-Options' => 'nosniff',
 		'X-XSS-Protection' => '1; mode=block',
 		'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains;',
-		'HSTS' => 'max-age=31536000; includeSubDomains;'
+		'Cache-Control' => 'public; max-age=31536000; immutable;',
 	];
 
 	/**
